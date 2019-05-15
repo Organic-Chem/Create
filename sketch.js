47432853a46aee4517f715422d3c6940a74cd9ce
Organic-Chem <47325282+Organic-Chem@users.noreply.github.com>
2019-05-15 19:19:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,25 +3,9 @@ var startY = 0;
 var endX = 0;
 var endY = 0;
 
-//var black = color(0, 0, 0);
-//var white = color(255, 255, 255);
-
 function setup() {
 	createCanvas(640, 480);
 }
-//function draw(){
-//	line(startX, startY, endX, endY);
-//}
-//function mouseDragged(){
-	//fill(white);
-	//line(startX, startY, endX, endY);
-
-	//endX = mouseX;
-	//endY = mouseY;
-
-	//fill(black);
-	//line(startX, startY, endX, endY);
-//}
 function mousePressed(){
 	startX = mouseX;
 	startY = mouseY;
