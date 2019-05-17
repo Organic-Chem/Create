5cafbafcd009ace2ef0795394d13f23d0daef693
Organic-Chem <47325282+Organic-Chem@users.noreply.github.com>
2019-05-17 09:57:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,9 +10,16 @@ var black = color(0, 0, 0);
 var white = color(255, 255, 255);
 
 var lineButton;
+var button;
 
 function setup() {
 	createCanvas(640, 480);
+
+	background(0);
+
+	button = createButton('click me');
+	button.position(19, 19);
+	button.mousePressed(changeBG);
 
 	lineButton = createButton('Draw Line');
 	lineButton.position(0, 10);
@@ -21,6 +28,10 @@ function setup() {
 	//eraseButton = createButton('Erase');
 	//eraseButton.position(0, 80);
 	//eraseButton.mousePressed(erase);
+}
+function changeBG(){
+	var val = random(255);
+	background(val);
 }
 function line(){
 	toLine = true;
